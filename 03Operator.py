b79051ff9ace80eb52e3fd3c968c6ce158f71d2b
DESKTOP-PVVSJ3E\kosmo <fins6789@gmail.com>
2023-05-12 11:36:54 +0900

    0512 python basic

---
+++ 03Operator.py
@@ -0,0 +1,25 @@
+#
+#
+num1 = 100
+num2 = 20
+
+print('add', num1+num2)
+print('sub', num1-num2)
+print('mul', num1*num2)
+print('div', num1/num2)
+print('rem', num1%num2)
+print('quo', num1//num2)
+print('squ', num1**num2)
+
+#관계연산자, 논리연산자
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
+print(num1==num2)
+print(num1!=num2)
+print(num1>=50 and num2<=10)
+print(num1>=50 or num2<=10)
+print(not(num1>=50))
+
+
